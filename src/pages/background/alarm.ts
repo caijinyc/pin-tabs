430b3a2751057c1f2c0bac213399017e9b960101
jin <me@caijin.tech>
2024-02-23 10:23:47 +0800

    feat: remove backup alarm
    (cherry picked from commit ed868c6265e838244214168ce4d68b5d6ff45f22)

--- src/pages/background/alarm.ts
+++ src/pages/background/alarm.ts
@@ -24,36 +24,36 @@ loadDataFromOtherDeviceFn();
 chrome.alarms.onAlarm.addListener(function (alarm) {
   console.log('onAlarm', alarm, alarm.name, alarm.scheduledTime, alarm.periodInMinutes);
 
-  if (alarm.name === BACKUP_DATA) {
-    console.log('执行备份操作', dayjs().format('YYYY-MM-DD HH:mm:ss'));
-    const startBackup = async () => {
-      const localStorageData = await storeLocalStorage.get();
-      if (localStorageData.alreadyBackupToGist) {
-        console.log('already synced to gist, skip');
-        return;
-      }
-
-      storeLocalStorage.set({
-        ...localStorageData,
-        alreadyBackupToGist: true,
-      });
-
-      delete localStorageData.alreadyBackupToGist;
-
-      return backupToGist({
-        ...localStorageData,
-        selectedIndex: 0,
-      });
-    };
-
-    startBackup()
-      .then(() => {
-        console.log('backup success');
-      })
-      .catch(e => {
-        console.log('backup fail', e);
-      });
-  }
+  // if (alarm.name === BACKUP_DATA) {
+  //   console.log('执行备份操作', dayjs().format('YYYY-MM-DD HH:mm:ss'));
+  //   const startBackup = async () => {
+  //     const localStorageData = await storeLocalStorage.get();
+  //     if (localStorageData.alreadyBackupToGist) {
+  //       console.log('already synced to gist, skip');
+  //       return;
+  //     }
+  //
+  //     storeLocalStorage.set({
+  //       ...localStorageData,
+  //       alreadyBackupToGist: true,
+  //     });
+  //
+  //     delete localStorageData.alreadyBackupToGist;
+  //
+  //     return backupToGist({
+  //       ...localStorageData,
+  //       selectedIndex: 0,
+  //     });
+  //   };
+  //
+  //   startBackup()
+  //     .then(() => {
+  //       console.log('backup success');
+  //     })
+  //     .catch(e => {
+  //       console.log('backup fail', e);
+  //     });
+  // }
 
   if (alarm.name === syncDataToGist) {
     console.log(`执行 ${syncDataToGist} 操作`, dayjs().format('YYYY-MM-DD HH:mm:ss'));
